e2e8cd68cbcef6fc2ab20bf0a71f042ed7a0b8af
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-11-09 14:56:07 +1100

    make index page  responsive

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,11 @@
  import { handleSubmit } from './js/formHandle'
  import { checkForDateInput } from './js/userInputCheck'
 
+ import './styles/base.scss'
+ import './styles/header.scss'
+ import './styles/main.scss'
+ import './styles/footer.scss'
+ 
  document.getElementById('submit').addEventListener('click',displayData);
 
  function displayData(){
