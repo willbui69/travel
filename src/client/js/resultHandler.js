7367bfc8469da393d7da2d0a11cd4c7499ed75f5
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-11-11 21:25:18 +1100

    add margin-left for updated data

--- src/client/js/resultHandler.js
+++ src/client/js/resultHandler.js
@@ -4,12 +4,12 @@ async function insertServerData() {
 
     //Update the UI
     document.getElementById('result-image').innerHTML = `<img style="width: 100vw; height: 30vh;" src="${serverData.imageLink}" alt="${serverData.cityName} city">`
-    document.getElementById('result-destination').innerHTML = `<p style="font-family:'Times New Roman'; font-size: 24px; color: white;"><b>My trip to: ${serverData.cityName}<br>
+    document.getElementById('result-destination').innerHTML = `<p style="font-family:'Times New Roman'; font-size: 24px; color: white; margin-left: 5px;"><b>My trip to: ${serverData.cityName}<br>
                                                                Departing: ${serverData.departDate} </b></p>`
-    document.getElementById('result-dategap').innerHTML = `<p style="font-family:'Times New Roman'; font-size: 20px; color: white;">${serverData.cityName}, ${serverData.countryName} is ${serverData.dateGap} days away</p>`
-    document.getElementById('result-weather').innerHTML = `<p style="font-family:'Times New Roman'; font-size: 20px; color: white;">Typical weather for then is:<br>
-                                                            <span style="font-family:'Times New Roman'; font-size: 18px; color: white;">Temperature: ${serverData.temp}</span><br>
-                                                            <span style="font-family:'Times New Roman'; font-size: 18px; color: white;">${serverData.des} throughout the day</span></p>`
+    document.getElementById('result-dategap').innerHTML = `<p style="font-family:'Times New Roman'; font-size: 20px; color: white; margin-left: 5px;">${serverData.cityName}, ${serverData.countryName} is ${serverData.dateGap} days away</p>`
+    document.getElementById('result-weather').innerHTML = `<p style="font-family:'Times New Roman'; font-size: 20px; color: white; margin-left: 5px;">Typical weather for then is:<br>
+                                                            <span style="font-family:'Times New Roman'; font-size: 18px; color: white; margin-left: 5px;">Temperature: ${serverData.temp}</span><br>
+                                                            <span style="font-family:'Times New Roman'; font-size: 18px; color: white; margin-left: 5px;">${serverData.des} throughout the day</span></p>`
 }
 
 const getData = async(url='')=>{
