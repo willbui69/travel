ba63aa5b8b9428bc90d13f814e31d6f0639410bc
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-11-20 11:06:01 +1100

    add app.test.js file to test the app

--- src/server/app.js
+++ src/server/app.js
@@ -2,4 +2,23 @@ const express = require('express')
 
 const app = express()
 
-export default app
+app.use(express.static('dist'))
+
+/* Middleware*/
+const bodyParser = require('body-parser');
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+console.log(__dirname)
+
+// Cors for cross origin allowance
+const cors = require('cors');
+const { response } = require('express');
+app.use(cors());
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+module.exports = app;
--- src/server/index.js
+++ src/server/index.js
@@ -1,37 +1,15 @@
-var path = require('path')
-const express = require('express')
+const app = require('./app.js')
 const fetch = require('node-fetch')
 const dotenv = require('dotenv')
 dotenv.config();
-
-//Data project endpoint
-projectData = {};
-
-const app = express()
-
-app.use(express.static('dist'))
-
-/* Middleware*/
-const bodyParser = require('body-parser');
-
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-console.log(__dirname)
-
-// Cors for cross origin allowance
-const cors = require('cors');
-const { response } = require('express');
-app.use(cors());
-
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
 
 // designates what port the app will listen to for incoming requests
 app.listen(3000, function () {
     console.log('The travel app listening on port 3000!')
 })
+
+//Data project endpoint
+projectData = {};
 
 //Get user input from client side
 app.post('/post', async (req, res)=>{
