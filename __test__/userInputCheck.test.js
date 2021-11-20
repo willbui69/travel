475731055af9d9175ff69d1ebb742b0bc82b16f5
willbui69 <76840668+willbui69@users.noreply.github.com>
2021-11-20 22:05:35 +1100

    add city name test

--- __test__/userInputCheck.test.js
+++ __test__/userInputCheck.test.js
@@ -1,8 +1,13 @@
 import { checkForStartDate } from "../src/client/js/userInputCheck";
+import { checkForCityInput } from "../src/client/js/userInputCheck";
 
 describe("Testing the user input", ()=>{
     test("Testing the start date", ()=>{
         const startDate = '2021-11-26';
         expect(checkForStartDate(startDate)).toBeTruthy();
+    })
+    test("Testing the city name", ()=>{
+        const cityName = 'Ha Noi';
+        expect(checkForCityInput(cityName)).toBeTruthy();
     })
 })
